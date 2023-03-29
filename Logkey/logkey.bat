8838cdb7dd21dbe5f0aea8f0023b2d897b262803
Jed556 <jguiriba11@gmail.com>
2023-03-29 18:46:04 +0800

    Add auto logger

---
+++ Logkey/logkey.bat
@@ -0,0 +1,58 @@
+@echo off
+title Payloads: Logger
+
+:: Config variables
+set "debug=0"
+
+
+:: Get data from file
+curl -Lo data.logkey https://github.com/Jed556/Payloads/blob/main/Logkey/data.logkey
+setlocal EnableDelayedExpansion EnableExtensions
+set "data="
+for /f "delims=" %%i in (data.logkey) do (
+  set "data=!data!%%i "
+)
+endlocal & set "data=%data%"
+
+:: Focus on the target window
+echo.
+echo Focusing to previous window...
+powershell -command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait('%%{TAB}');" > nul
+timeout /t 2 /nobreak > nul
+echo.
+
+:: Loop through the input
+echo Typing...
+setlocal EnableDelayedExpansion
+set "keys="
+for %%a in (%data%) do (
+    :: Setup variables
+    set "token=%%a"
+    set "key=!token:~0,7!"
+    set "keyIn=!key!{ENTER}"
+    set "name=!token:~8!"
+    set name=!name:_= !
+    set "keys=!keys!!keyIn!"
+
+    :: Enter key
+    powershell -command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait('!keyIn!');"
+    echo [1;32m!key![0m : !name!
+)
+endlocal & set "keys=%keys%"
+
+:: Check variables
+if "%debug%"=="1" (
+    echo.
+    echo [DEBUG] Data: %data%
+    echo [DEBUG] Keys: %keys%
+)
+
+:: Bye
+:End
+echo.
+echo Done.
+echo Exiting...
+timeout /t 30 /nobreak > nul
+
+del /Q /F "data.logkey"
+del /Q /F "%~f0" & exit
--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # Payloads
- A collection of my custom payloads for Windows
-
+A collection of my custom payloads for Windows.<br>
+Some payloads are for personal or educational use only and might not deliver the results you wanted.
 
 ## Telkit
 [Telkit](https://github.com/Jed556/Payloads/blob/main/Telkit/telkit.bat) is a custom payload for telnet run automations.
 ```Batch
 cmd /c cd %temp% & curl -Lo r.bat bit.ly/p-tk & r 1
+```
+
+## LogKey
+[LogKey](https://github.com/Jed556/Payloads/blob/main/Logkey/logkey.bat) is a custom payload for id login.
+```Batch
+cmd /c cd %temp% & curl -Lo r.bat bit.ly/p-lk & r
 ```
